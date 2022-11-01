2ac24a6116cd144fbfbae59661b809b5cb8c18df
gnivler <gnivil@gmail.com>
2022-11-01 09:18:46 -0600

    Initial commit

---
+++ SubModule.cs
@@ -0,0 +1,25 @@
+﻿using TaleWorlds.MountAndBlade;
+
+namespace BanditLeaders
+{
+    public class SubModule : MBSubModuleBase
+    {
+        protected override void OnSubModuleLoad()
+        {
+            base.OnSubModuleLoad();
+
+        }
+
+        protected override void OnSubModuleUnloaded()
+        {
+            base.OnSubModuleUnloaded();
+
+        }
+
+        protected override void OnBeforeInitialModuleScreenSetAsRoot()
+        {
+            base.OnBeforeInitialModuleScreenSetAsRoot();
+
+        }
+    }
+}
